ef2f50ede1d9e04f61748cafa599d28d4403148d
Alan Truong <Alan.Truong@microenla.com>
2018-07-30 16:19:31 -0700

    Add argument to toggle using distance api.

--- main.py
+++ main.py
@@ -11,11 +11,20 @@ import output_formatting
 import wunderground
 
 
-def parse_args():
+def __parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-w", "--weather",
                         action="store_true",
+                        help="Suggest based on current weather.",
                         dest="use_weather")
+    parser.add_argument("-m", "--multiple",
+                        nargs="?",
+                        help="Suggest MULTIPLE restaurants at once",
+                        const=1)
+    parser.add_argument("-d", "--distance",
+                        action="store_true",
+                        help="Look up travel time.",
+                        dest="use_distance")
 
     arg_group = parser.add_mutually_exclusive_group(required=False)
     arg_group.add_argument("-c", "--choices",
@@ -30,7 +39,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def get_temperature():
+def __try_get_temperature():
     weather_data = wunderground.get_weather_data()
     if not weather_data:
         print("Trouble getting weather data. Won't be able to recommend based on weather.")
@@ -39,7 +48,7 @@ def get_temperature():
         return float(weather_data[wunderground.temperature_key])
 
 
-def get_travel_time(restaurant):
+def __try_get_travel_time(restaurant):
     travel_time = google_distance.get_travel_time(restaurant, start_address)
     if not travel_time:
         print("Trouble getting travel time.")
@@ -48,21 +57,57 @@ def get_travel_time(restaurant):
         return travel_time
 
 
+def __get_n_suggestions(restaurants, filters, temperature, n):
+    suggestions = []
+    restaurants_copy = []
+    restaurants_copy.extend(restaurants)
+    while len(suggestions) < n:
+        suggestion = suggest.suggest(restaurants_copy, filters, temperature)
+        suggestions.append(suggestion)
+        restaurants_copy.remove(suggestion)
+
+    return suggestions
+
+
+def __try_get_selection_at_index(index, suggested_restaurants):
+    try:
+        return suggested_restaurants[int(index)-1]
+    except:
+        print("Invalid input.")
+        return None
+
+
+def __generate_output_for_suggestions(suggested_restaurants, use_distance=None):
+    output = []
+    indexer_for_convenience = 1
+    for suggested_restaurant in suggested_restaurants:
+        travel_time = __try_get_travel_time(suggested_restaurant) if use_distance else None
+        output.extend(output_formatting.generate_suggestion_strings(suggested_restaurant, temperature, travel_time))
+        output.append(str.format("({0})", indexer_for_convenience))
+        output.append("")
+        indexer_for_convenience += 1
+
+    return output
+
+
 restaurant_json = file_read_write.read_json(restaurants_path)
 restaurants = [Restaurant.from_dict(r) for r in restaurant_json]
-arguments = parse_args()
+arguments = __parse_args()
 
-if arguments.choices is None:
-    temperature = get_temperature() if arguments.use_weather else None
-    suggested_restaurant = suggest.suggest(restaurants, arguments.filters, temperature)
-    travel_time = get_travel_time(suggested_restaurant)
-    if suggested_restaurant:
-        output = output_formatting.generate_suggestion_strings(suggested_restaurant, temperature, travel_time)
+if not arguments.choices:
+    temperature = __try_get_temperature() if arguments.use_weather else None
+    suggested_restaurants = __get_n_suggestions(restaurants, arguments.filters, temperature, 3)
+    if suggested_restaurants:
+        output = __generate_output_for_suggestions(suggested_restaurants, arguments.use_distance)
         for line in output:
             print(line)
-        accepted = input("Are you going to go here? (Y/N)")
-        if accepted.lower() == "y":
-            suggested_restaurant.last_visit = datetime.datetime.now()
+
+        selected = input("Select index of choice: (index/N)")
+        selected_restaurant = __try_get_selection_at_index(selected, suggested_restaurants)\
+            if selected.lower() != "n"\
+            else None
+        if selected_restaurant:
+            selected_restaurant.last_visit = datetime.datetime.now()
             output = {"restaurants": [r for r in restaurants]}
             file_read_write.write_json(restaurants_path, output)
     else:
--- output_formatting.py
+++ output_formatting.py
@@ -2,13 +2,13 @@ def generate_suggestion_strings(selected_restaurant, temperature, travel_time):
     statements = [str.format("Since it feels like {0}F outside", temperature),
                   str.format("You should go to: {0}", selected_restaurant.name),
                   str.format("It has a time factor of {0}", selected_restaurant.time),
-                  str.format("and a distance factor of {0}", selected_restaurant.distance),
-                  str.format("it will take {0} to get there", travel_time),
-                  str.format("You last visited {0}", selected_restaurant.last_visit)
+                  str.format("You last visited {0}", selected_restaurant.last_visit.strftime("%A, %m/%d/%y"))
                   if selected_restaurant.last_visit is not None
                   else "You have never been!"]
 
     if temperature is None:
         statements = statements[1:]
+    if travel_time:
+        statements.append(str.format("it will take {0} to get there", travel_time))
 
     return statements
